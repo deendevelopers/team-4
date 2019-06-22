b76574202796962b276824971c7c89f68e0cb652
Yosuf Ali <yosufali@users.noreply.github.com>
2019-06-22 21:44:24 +0100

    Add and fetch dummy data from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,25 @@ import './App.css'
 import Search from './Search'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
   onSearch = (value) => {
     console.log(`Search triggered with value ${value}`)
     // make API call
+    fetch('http://0.0.0.0:5000/api')
+    .then((resp) => resp.json())
+    .then((data)=> {
+      this.setState({data: data})
+    })
+    .catch((error) => {
+      // handle any server errors
+      console.log('error: ', error)
+    })
   }
 
   render() {
--- serve.py
+++ serve.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import cross_origin
 
 app = Flask(__name__, static_folder="frontend/build/static", template_folder="frontend/build")
 
@@ -7,8 +8,26 @@ def hello():
     return render_template('index.html')
 
 @app.route('/api')
+@cross_origin()
 def api():
-  return 'some json data'
+  data = [
+    {
+      'type': 'hadtih',
+      'chapterNo': 1,
+      'hadtihNo': 2,
+      'chapter': 'Revelation - كتاب بدء الوحى',
+      'textAr': 'حدثنا عبد الله بن يوسف، قال أخبرنا مالك، عن هشام بن عروة، عن أبيه، عن عائشة أم المؤمنين  رضى الله عنها  أن الحارث بن هشام  رضى الله عنه  سأل رسول الله صلى الله عليه وسلم فقال يا رسول الله كيف يأتيك الوحى فقال رسول الله صلى الله عليه وسلم " أحيانا يأتيني مثل صلصلة الجرس  وهو أشده على  فيفصم عني وقد وعيت عنه ما قال، وأحيانا يتمثل لي الملك رجلا فيكلمني فأعي ما يقول ". قالت عائشة رضى الله عنها ولقد رأيته ينزل عليه الوحى في اليوم فيفصم عنه وإن جبينه ليتفصد عرقا.',
+      'textEn': 'Narrated \'Aisha:(the mother of the faithful believers) Al-Harith bin Hisham asked Allah\'s Apostle "O Allah\'s Apostle! How is the Divine Inspiration revealed to you?" Allah\'s Apostle replied, "Sometimes it is (revealed) like the ringing of a bell, this form of Inspiration is the hardest of all and then this state passes off after I have grasped what is inspired. Sometimes the Angel comes in the form of a man and talks to me and I grasp whatever he says." \'Aisha added: Verily I saw the Prophet being inspired divinely on a very cold day and noticed the sweat dropping from his forehead (as the Inspiration was over).'
+    },
+    {
+      'type': 'verse',
+      'surahNo': 1,
+      'verseNo': 2,
+      'textAr': 'الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ',
+      'textEn': 'All praise is due to Allah, Lord of the worlds',
+    }
+  ]
+  return jsonify(data)
 
 print('Starting Flask!')
 
