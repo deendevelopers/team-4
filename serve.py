f363e84abe68175f02d8cf7ef7a3b89a7de84bfe
Yosuf Ali <yosufali@users.noreply.github.com>
2019-06-22 19:31:18 +0100

    Clean up serve file

--- serve.py
+++ serve.py
@@ -1,23 +1,16 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# # app = Flask(__name__, static_folder="frontend/build/static", template_folder="build")
-
-# @app.route('/')
-# def index():
-#   return 'Hello, World!'
-
-# @app.route('/api')
-# def api():
-#   return 'kjbkjsgblksdgkdsf'
-
-
 from flask import Flask, render_template
+
 app = Flask(__name__, static_folder="frontend/build/static", template_folder="frontend/build")
+
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route('/api')
+def api():
+  return 'some json data'
+
 print('Starting Flask!')
+
 app.debug=True
 app.run(host='0.0.0.0')
