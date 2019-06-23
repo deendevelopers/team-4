283e598ce46047bc1ea2f0d49e6f5c579e7e2f24
Yosuf Ali <yosufali@users.noreply.github.com>
2019-06-23 17:24:30 +0100

    Add links to quran and sunnah website

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -7,8 +7,38 @@ export default class Card extends Component {
   render() {
     const cardData = this.props.data
 
+    let url 
+
+    if (cardData.type === 'hadith') {
+      switch (cardData.source) {
+        case 'Sahih Bukhari':
+          url = `https://sunnah.com/bukhari/${cardData.chapterNo}/${cardData.hadithNo}`
+          break
+        case 'Sahih Muslim':
+          url = `https://sunnah.com/muslim/${cardData.chapterNo}/${cardData.hadithNo}`
+          break
+        case 'Sunan Abi Da\'ud':
+          url = `https://sunnah.com/abudawud/${cardData.chapterNo}/${cardData.hadithNo}`
+          break
+        case 'Jami\' al-Tirmidhi':
+          url = `https://sunnah.com/tirmidhi/${cardData.chapterNo}/${cardData.hadithNo}`
+          break
+        case 'Sunan an-Nasa\'i':
+            url = `https://sunnah.com/nasai/${cardData.chapterNo}/${cardData.hadithNo}`
+            break
+        case 'Sunan Ibn Majah':
+          url = `https://sunnah.com/ibnmajah/${cardData.chapterNo}/${cardData.hadithNo}`
+          break
+        default:
+          url = '#'
+      }
+    }
+    else if (cardData.type === 'verse') {
+      url = `https://quran.com/${cardData.surahNo}/${cardData.verseNo}`
+    }
+
     return (
-      <div className="card-container">
+      <a href={url} target="__blank" className="card-container">
         <div className="card-meta">
           {cardData && cardData.type === 'hadith' ? <span>Chapter #: {cardData.chapterNo}</span> : <span>Surah #: {cardData.surahNo}</span>}
           {cardData && cardData.type === 'hadith' ? <span>Hadith #: {cardData.hadithNo}</span> : <span>Verse: # {cardData.verseNo}</span>}
@@ -17,7 +47,7 @@ export default class Card extends Component {
           <div className="card-arabic">{cardData.textAr}</div>
           <div className="card-english">{cardData.textEn}</div>
         </div>
-      </div>
+      </a>
     );
   }
 }
